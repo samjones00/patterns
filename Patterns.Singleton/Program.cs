88d1a467284d63720d175079116c3aa589fa5d01
sam@smjdesign.co.uk <sam@smjdesign.co.uk>
2020-11-13 21:01:26 +0000

    Adding singleton pattern

---
+++ Patterns.Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Patterns.Singleton
+{
+    class Program
+    {
+        //lazy singleton
+        private static readonly Lazy<SingletonClass> lazySingletonClass = new Lazy<SingletonClass>(() => new SingletonClass());
+
+        public static SingletonClass LazySingletonClass => lazySingletonClass.Value;
+
+        //shortened singleton
+        private static SingletonClass _shortenedingletonClass;
+        private static readonly SingletonClass ShortenedSingletonClass = _shortenedingletonClass ?? new SingletonClass();
+
+        //classic singleton
+        private static SingletonClass _singletonClass;
+        private static SingletonClass SingletonClass
+        {
+            get
+            {
+                if (_singletonClass == null)
+                {
+                    _singletonClass = new SingletonClass();
+                    return _singletonClass;
+                }
+
+                return _singletonClass;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var singleton = SingletonClass;
+            singleton.DoAction();
+
+            var shortenedSingleton = ShortenedSingletonClass;
+            shortenedSingleton.DoAction();
+
+            var lazySingleton = LazySingletonClass;
+            lazySingleton.DoAction();
+
+            lazySingleton.DoAction();
+        }
+    }
+}
---
+++ Patterns.Singleton/SingletonClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Patterns.Singleton
+{
+    public class SingletonClass
+    {
+        public DateTime DateInitiated = DateTime.Now;
+
+        public void DoAction()
+        {
+            Console.WriteLine($"Date initiated: {DateInitiated}");
+        }
+    }
+}
