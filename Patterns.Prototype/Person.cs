d6df3f89a3d009f071a56cd12154b5c7a93d99a3
sam <sam@smjdesign.co.uk>
2020-11-23 23:32:30 +0000

    Rearranging folders

--- Patterns.Prototype/Person.cs
+++ Patterns.Prototype/Person.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Patterns.Creational.Prototype
+namespace Patterns.Prototype
 {
     public class Person : ICloneable
     {
